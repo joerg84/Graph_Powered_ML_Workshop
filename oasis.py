d7c7495279a07a94cab24f9497e3af9c24d0c091
joerg84 <joerg.schad@gmail.com>
2020-04-23 18:11:52 +0200

    Made database connection more reliable.

--- oasis.py
+++ oasis.py
@@ -1,34 +1,62 @@
 import json
 import requests
 import sys
+import time
+
+from pyArango.connection import *
 
 # retrieving credentials from ArangoDB tutorial service
 def getTempCredentials():
-  with open("creds.dat","r+") as cacheFile: 
-    contents = cacheFile.readline()
-    if len(contents) > 0:
-      #. check if credentials are still valid
-      login = json.loads(contents)
-      url = "https://"+login["hostname"]+":"+str(login["port"])
-      conn =""
-      try: 
-        conn = Connection(arangoURL=url, username=login["username"], password=login["password"],)
-        print("Reusing cached credentials.")
-        return login
-      except:
-        print("Credentials expired.")
-        pass # Ignore and retrieve new 
+    with open("creds.dat","r+") as cacheFile: 
+        contents = cacheFile.readline()
+        if len(contents) > 0:
+            login = None
+            url = ""
+           
+            # check if credentials are still valid
+            try:
+                login = json.loads(contents)
+                url = "https://"+login["hostname"]+":"+str(login["port"])
+            except:
+                # Incorrect data in cred file and retrieve new credentials
+                cacheFile.truncate(0) 
+                pass
+        
+            conn =""
+            if (login is not None):
+                try: 
+                    conn = Connection(arangoURL=url, username=login["username"], password=login["password"],)
+                    print("Reusing cached credentials.")
+                    return login
+                except:
+                    print("Credentials expired.")
+                    pass # Ignore and retrieve new 
     
-    # Retrieve new credentials from Foxx Service
-    print("Requesting new temp credentials.")
-    url = 'https://5904e8d8a65f.arangodb.cloud:8529/_db/_system/alpha/tutorialDB'
-    x = requests.post(url, data = "{}")
+        # Retrieve new credentials from Foxx Service
+        print("Requesting new temp credentials.")
+        url = 'https://5904e8d8a65f.arangodb.cloud:8529/_db/_system/alpha/tutorialDB'
+        x = requests.post(url, data = "{}")
 
-    if x.status_code != 200:
-      print("Error retrieving login data.")
-      sys.exit()
-    # Caching credentials
-    cacheFile.truncate(0) 
-    cacheFile.write(x.text)
-    print("Temp database ready to use.")
-  return json.loads(x.text)
+        if x.status_code != 200:
+            print("Error retrieving login data.")
+            sys.exit()
+        # Caching credentials
+        cacheFile.truncate(0) 
+        cacheFile.write(x.text)
+        print("Temp database ready to use.")
+        return json.loads(x.text)
+
+# Connect against an oasis DB and return pyarango connection
+def connect(login):
+    url = "https://"+login["hostname"]+":"+str(login["port"])
+    conn = None
+    try:
+        conn = Connection(arangoURL=url, username=login["username"], password=login["password"],)
+    except:
+        time.sleep(1)
+        conn = Connection(arangoURL=url, username=login["username"], password=login["password"],)
+    return conn
+        
+    
+    
+    
